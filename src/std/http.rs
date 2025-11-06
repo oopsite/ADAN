c4a249135bbb021e2475d247f165cb8464d32cd4
lily <ilovesuno@proton.me>
2025-11-06 12:48:38 -0700

    chore: start the HTTP library (std)

---
+++ src/std/http.rs
@@ -0,0 +1,24 @@
+pub enum HTTP {
+    Get,
+    Post,
+    GetSync,        // Is functionally identical to  Get, although only synchronous.
+    PostSync,       // Is functionally identical to Post, although only synchronous.
+}
+
+impl HTTP {
+    pub fn get() {
+
+    }
+
+    pub fn post() {
+
+    }
+
+    pub fn get_sync() {
+
+    }
+
+    pub fn post_sync() {
+        
+    }
+}
